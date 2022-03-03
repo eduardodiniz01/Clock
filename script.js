39818d0841c3a8cf8f20b6ee00d30efa0b73104f
Eduardo <74016606+eduardodiniz01@users.noreply.github.com>
2022-03-03 11:37:58 -0300

    Update script.js

--- script.js
+++ script.js
@@ -10,10 +10,10 @@ const hourEl = document.querySelector('.hour')
    const html = document.querySelector('html')  
    if (html.classList.contains('dark')) {  
      html.classList.remove('dark')  
-     e.target.innerHTML = 'Dark mode'  
+     e.target.innerHTML = 'Dark Mode'  
    } else {  
      html.classList.add('dark')  
-     e.target.innerHTML = 'Light mode'  
+     e.target.innerHTML = 'Light Mode'  
    }  
  })  
  function setTime() {  
